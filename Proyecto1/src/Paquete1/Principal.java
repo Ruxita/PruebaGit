1843dbe89aabfcf7180ea94027d691948de66c73
romer <romer@Ninosauria>
2024-03-12 16:04:41 +0100

    version2

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -3,6 +3,7 @@ package Paquete1;
 public class Principal {
     public static void main (String []args) {
 	System.out.println("Probando repositorio Git en 1º DAM");
+	System.out.println("Hola y A ver si funciona");
 	System.out.println("Adios");
     }
 
