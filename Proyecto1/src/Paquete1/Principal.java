f7b2916ece5f9a15f7dcf6a43b5644bcf3369618
romer <romer@Ninosauria>
2024-03-12 15:55:15 +0100

    repositorio

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -1,5 +1,9 @@
 package Paquete1;
 
 public class Principal {
+    public static void main (String []args) {
+	System.out.println("Probando repositorio Git en 1º DAM");
+	System.out.println("Adios");
+    }
 
 }
